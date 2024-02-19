b51399cd83681482d18f95fc56623dc1157303e0
ybond93 <y.bahem@gmail.com>
2024-02-20 00:20:46 +0100

    init commit

---
+++ app/src/main/java/miun/dt170g/application_restaurant/MainActivity.java
@@ -0,0 +1,14 @@
+package miun.dt170g.application_restaurant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

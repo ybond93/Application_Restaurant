ffa22447a55e6dff93a45f56b2e31a17cbce266e
Luwam Amine <luwam.amine@outlook.com>
2024-03-01 15:14:52 +0100

    commit initt

--- app/src/main/java/miun/dt170g/application_restaurant/MainActivity.java
+++ app/src/main/java/miun/dt170g/application_restaurant/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this)); // Set the LayoutManager
 
--- app/src/main/java/miun/dt170g/application_restaurant/retrofit/RetrofitInterface.java
+++ app/src/main/java/miun/dt170g/application_restaurant/retrofit/RetrofitInterface.java
@@ -11,6 +11,7 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 
+
 public interface RetrofitInterface {
     @GET("events")
     Call<ArrayList<Event>> getEvents();
